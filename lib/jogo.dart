809d6095d881ea019ee0cc6cd786ca871a00deb7
Richardy_Rodrigues <richardyrodrigues519@gmail.com>
2025-03-24 23:32:19 +0000

    Modificado o modo de alinhamento para center no body

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -18,7 +18,7 @@ class _JogoState extends State<Jogo> {
         title: const Text('JokenPo'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Padding(
             padding: EdgeInsets.only(top: 32, bottom: 16),
@@ -28,7 +28,34 @@ class _JogoState extends State<Jogo> {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
+          // Imagem padrão de escolha do app
           Image(image: AssetImage('images/padrao.png')),
+
+          Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Escolha uma opção abaixo",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image(
+                image: AssetImage('images/pedra.png'),
+                height: 100,
+              ),
+              Image(
+                image: AssetImage('images/papel.png'),
+                height: 100,
+              ),
+              Image(
+                image: AssetImage('images/tesoura.png'),
+                height: 100,
+              ),
+            ],
+          )
         ],
       ),
     );
